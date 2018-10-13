67760d7ba65d5da137cd55f29de57be969130f70
paulpou <zapaja.gaming@gmail.com>
2018-10-13 10:23:26 +0300

    added tank

--- megadojo2018/Assets/Scripts/Movement.cs
+++ megadojo2018/Assets/Scripts/Movement.cs
@@ -4,9 +4,39 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour {
 
+    public float speed = 1.0f;
 
-	void Update ()
+    void Start()
     {
-		
-	}
+   
+    }
+
+
+    void Update ()
+    {
+
+        if (Input.GetKey(KeyCode.A) == true)
+        {
+            gameObject.GetComponent<Transform>().position -= new Vector3(speed, 0.0f, 0.0f);
+        }
+
+        if (Input.GetKey(KeyCode.D) == true)
+        {
+            gameObject.GetComponent<Transform>().position += new Vector3(speed, 0.0f, 0.0f);
+
+        }
+
+        if (Input.GetKey(KeyCode.S) == true)
+        {
+            gameObject.GetComponent<Transform>().position -= new Vector3(0.0f, speed, 0.0f);
+        }
+
+        if (Input.GetKey(KeyCode.W) == true)
+        {
+            gameObject.GetComponent<Transform>().position += new Vector3(0.0f, speed, 0.0f);
+
+        }
+
+
+    }
 }
---
+++ megadojo2018/Assets/Scripts/Turret.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 mouse_pos = Input.mousePosition;
+        mouse_pos.z = 5.23f; //The distance between the camera and object
+        Vector3 object_pos = Camera.main.WorldToScreenPoint(gameObject.GetComponent<Transform>().position);
+        mouse_pos.x = mouse_pos.x - object_pos.x;
+        mouse_pos.y = mouse_pos.y - object_pos.y;
+        float angle = Mathf.Atan2(mouse_pos.y, mouse_pos.x) * Mathf.Rad2Deg;
+        Debug.Log(angle);
+        gameObject.GetComponent<Transform>().eulerAngles = new Vector3(0, 0, angle-180);
+    }
+}
